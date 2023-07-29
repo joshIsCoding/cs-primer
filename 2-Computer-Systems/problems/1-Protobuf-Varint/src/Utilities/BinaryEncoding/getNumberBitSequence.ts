f7f5b9bcd34571f5be5771b6d76f69b3d0fd3401
Josh Brown <yes_you_do@hotmail.com>
2023-07-29 18:49:23 +0100

    Remove console.log on each iteration

--- 2-Computer-Systems/problems/1-Protobuf-Varint/src/Utilities/BinaryEncoding/getNumberBitSequence.ts
+++ 2-Computer-Systems/problems/1-Protobuf-Varint/src/Utilities/BinaryEncoding/getNumberBitSequence.ts
@@ -10,7 +10,6 @@ function getNumberBitSequence(unsignedInt: number): BitStream {
   const bitSequence: BitStream = [];
 
   for (let i = unsignedInt; i >= 1; i = Math.floor(i / 2)) {
-    console.log(i);
     bitSequence.unshift(i % 2 === 0 ? '0' : '1');
   }
 
