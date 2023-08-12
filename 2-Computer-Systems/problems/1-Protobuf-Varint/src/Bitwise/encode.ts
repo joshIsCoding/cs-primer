a72bec0fa301c606b181ad6c9fa9849394f458fd
Josh Brown <yes_you_do@hotmail.com>
2023-08-12 20:26:27 +0100

    Add and improve handling for integers larger than 32 bits

--- 2-Computer-Systems/problems/1-Protobuf-Varint/src/Bitwise/encode.ts
+++ 2-Computer-Systems/problems/1-Protobuf-Varint/src/Bitwise/encode.ts
@@ -1,7 +1,12 @@
 import getHexString from './getHexString';
 import getVarIntByteArray from './getVarIntByteArray';
 
-function encode(num: number) {
+const sixtyFourBitMax = BigInt('0xffffffffffffffff');
+
+function encode(num: number | bigint) {
+  if (num < 0) throw new Error('Only unsigned integer encoding supported');
+  if (num > sixtyFourBitMax) throw new Error('Only integers less than the Uint64max supported');
+
   const varIntBytes = getVarIntByteArray(num);
 
   return getHexString(varIntBytes);
--- 2-Computer-Systems/problems/1-Protobuf-Varint/src/Bitwise/getVarIntByteArray.ts
+++ 2-Computer-Systems/problems/1-Protobuf-Varint/src/Bitwise/getVarIntByteArray.ts
@@ -1,9 +1,29 @@
-function getVarIntByteArray(num: number): Uint8Array {
-  const buffer = new Uint8Array(10); // array of 10 single-byte unsigned integers
-  const mask = 127; // 0111 1111 in binary
-  let truncatedNum = num;
+function getVarIntBytesForBigInt(number: bigint): Uint8Array {
+  let truncatedNum = number;
+  const mask = BigInt(127); // 0111 1111
+  const bigZero = BigInt(0);
+  const buffer = new Uint8Array(10);
+
   // for each of the 10 possible var-int bytes
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < buffer.length; i++) {
+    const willContinue = (truncatedNum & ~mask) !== bigZero;
+    const continuationBit = willContinue ? BigInt(128) : bigZero; // 1000 0000 OR 0000 0000
+    const nextVarIntByte = (truncatedNum & mask) | continuationBit;
+    buffer[i] = Number(BigInt.asUintN(8, nextVarIntByte));
+    if (!willContinue) break;
+
+    truncatedNum = truncatedNum >> BigInt(7);
+  }
+  return buffer;
+}
+
+function getVarIntBytesForNumber(number: number): Uint8Array {
+  const buffer = new Uint8Array(5);
+  let truncatedNum = number;
+  const mask = 127; // 0111 1111
+
+  // for each of the 10 possible var-int bytes
+  for (let i = 0; i < buffer.length; i++) {
     const willContinue = (truncatedNum & ~mask) !== 0;
     const continuationBit = willContinue ? 128 : 0; // 1000 0000 OR 0000 0000
     const nextVarIntByte = (truncatedNum & mask) | continuationBit;
@@ -12,8 +32,16 @@ function getVarIntByteArray(num: number): Uint8Array {
 
     truncatedNum = truncatedNum >> 7;
   }
-
   return buffer;
+}
+
+function getVarIntByteArray(num: number | bigint): Uint8Array {
+  // bitwise operations coerce operands into signed 32-bit numbers
+  const signed32BitMax = 2 ^ (31 - 1);
+
+  if (typeof num === 'number' && num <= signed32BitMax) return getVarIntBytesForNumber(num);
+
+  return getVarIntBytesForBigInt(BigInt(num));
 }
 
 export default getVarIntByteArray;
