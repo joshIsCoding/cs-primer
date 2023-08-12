0fe57cf2f82867998844f401f12c11fbc7c1751a
Josh Brown <yes_you_do@hotmail.com>
2023-08-12 20:26:41 +0100

    Implement Bitwise encoder in start file

--- 2-Computer-Systems/problems/1-Protobuf-Varint/src/start.ts
+++ 2-Computer-Systems/problems/1-Protobuf-Varint/src/start.ts
@@ -1,5 +1,9 @@
 import mathematicalEncode from './Mathematical/encode';
+import bitwiseEncode from './Bitwise/encode';
 
-console.log(` => ${mathematicalEncode(1)}`);
-console.log(` => ${mathematicalEncode(150)}`);
-console.log(` => ${mathematicalEncode(30000)}`);
+console.log(` Mathematical Encoding => ${mathematicalEncode(1)}`);
+console.log(` Bitwise Encoding => ${bitwiseEncode(1)}`);
+console.log(` Mathematical Encoding => ${mathematicalEncode(150)}`);
+console.log(` Bitwise Encoding => ${bitwiseEncode(150)}`);
+console.log(` Mathematical Encoding => ${mathematicalEncode(30000)}`);
+console.log(` Bitwise Encoding => ${bitwiseEncode(30000)}`);
