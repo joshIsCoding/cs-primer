bfc8a8907c0dee490006049894b05f33411c40c4
Josh Brown <yes_you_do@hotmail.com>
2023-08-18 14:37:08 +0100

    Remove mathematical encoding and add Number type-limited decoding

--- 2-Computer-Systems/problems/1-Protobuf-Varint/src/start.ts
+++ 2-Computer-Systems/problems/1-Protobuf-Varint/src/start.ts
@@ -1,9 +1,16 @@
-import mathematicalEncode from './Mathematical/encode';
 import bitwiseEncode from './Bitwise/encode';
+import decode from './Bitwise/decode';
 
-console.log(` Mathematical Encoding => ${mathematicalEncode(1)}`);
 console.log(` Bitwise Encoding => ${bitwiseEncode(1)}`);
-console.log(` Mathematical Encoding => ${mathematicalEncode(150)}`);
 console.log(` Bitwise Encoding => ${bitwiseEncode(150)}`);
-console.log(` Mathematical Encoding => ${mathematicalEncode(30000)}`);
 console.log(` Bitwise Encoding => ${bitwiseEncode(30000)}`);
+
+console.log(` Bitwise Encoding => ${bitwiseEncode(2 ** 31 - 1)}`); // Number-type encoding maximum
+
+console.log(` Bitwise Decoding => ${decode(bitwiseEncode(1))}`);
+
+console.log(` Bitwise Decoding => ${decode(bitwiseEncode(150))}`);
+
+console.log(` Bitwise Decoding => ${decode(bitwiseEncode(30000))}`);
+
+console.log(` Bitwise Decoding => ${decode('0xFFFFFFFF')}`); // Number-type decoding maximum
