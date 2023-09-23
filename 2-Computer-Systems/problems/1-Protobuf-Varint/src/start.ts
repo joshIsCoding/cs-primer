749c5fa087b06b0a2d85ded79505bf33eb3b6b08
Josh Brown <yes_you_do@hotmail.com>
2023-09-23 17:30:01 +0100

    Add additional edge cases and improve logs

--- 2-Computer-Systems/problems/1-Protobuf-Varint/src/start.ts
+++ 2-Computer-Systems/problems/1-Protobuf-Varint/src/start.ts
@@ -1,16 +1,22 @@
 import bitwiseEncode from './Bitwise/encode';
 import decode from './Bitwise/decode';
 
-console.log(` Bitwise Encoding => ${bitwiseEncode(1)}`);
-console.log(` Bitwise Encoding => ${bitwiseEncode(150)}`);
-console.log(` Bitwise Encoding => ${bitwiseEncode(30000)}`);
+console.log('Bitwise Encoding ----------------');
+console.log(` 1     => ${bitwiseEncode(1)}`);
+console.log(` 150   => ${bitwiseEncode(150)}`);
+console.log(` 30000 => ${bitwiseEncode(30000)}`);
 
-console.log(` Bitwise Encoding => ${bitwiseEncode(2 ** 31 - 1)}`); // Number-type encoding maximum
+console.log(` Number-type (32-bit) max => ${bitwiseEncode(2 ** 31 - 1)}`); // Number-type encoding maximum
+console.log(` Varint (64-bit) max      => ${bitwiseEncode(BigInt('0xFFFFFFFFFFFFFFFF'))}`);
 
-console.log(` Bitwise Decoding => ${decode(bitwiseEncode(1))}`);
+console.log('\nBitwise Decoding ----------------');
 
-console.log(` Bitwise Decoding => ${decode(bitwiseEncode(150))}`);
+console.log(` 0x01     => ${decode('0x01')}`);
+console.log(` 0x9061   => ${decode('0x9601')}`);
+console.log(` 0xb0ea01 => ${decode('0xb0ea01')}`);
 
-console.log(` Bitwise Decoding => ${decode(bitwiseEncode(30000))}`);
+console.log(` Max number-type decode => ${decode('0xFFFFFFFF')}`); // Number-type decoding maximum
 
-console.log(` Bitwise Decoding => ${decode('0xFFFFFFFF')}`); // Number-type decoding maximum
+console.log(` Min bigint decode      => ${decode('0x8080808001')}`); // Big int decoding threshold
+
+console.log(` Varint (64-bit) max    => ${decode('0xffffffffffffffffff01')}`); // Var int decoding maximum
