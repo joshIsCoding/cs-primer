bf20aee2929b1b4c3343acea0e8945016eceb680
Josh Brown <yes_you_do@hotmail.com>
2023-09-23 17:29:37 +0100

    Refactor for-loop conditions and fix byte-slice end index bug

--- 2-Computer-Systems/problems/1-Protobuf-Varint/src/Bitwise/encoding/getHexString.ts
+++ 2-Computer-Systems/problems/1-Protobuf-Varint/src/Bitwise/encoding/getHexString.ts
@@ -2,14 +2,15 @@ import { VarInt } from '../../varint';
 
 function getHexString(bytes: Uint8Array): VarInt {
   let hexStr = '';
-  let msbIndex = 0;
+  let msbIndex = 0; // position of most significant byte in little endian array
   for (let i = bytes.length - 1; i >= 0; i--) {
-    if (bytes[i] === 0) continue;
-
-    msbIndex = Math.min(bytes.length - 1, i + 1);
-    break;
+    if (bytes[i] !== 0) {
+      msbIndex = Math.min(bytes.length - 1, i + 1);
+      break;
+    }
+    continue;
   }
-  bytes.slice(0, msbIndex).forEach((uIntByte) => {
+  bytes.slice(0, msbIndex + 1).forEach((uIntByte) => {
     if (uIntByte === 0) return;
 
     hexStr += uIntByte.toString(16).padStart(2, '0');
