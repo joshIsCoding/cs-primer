f0fd44b5bbdad6be41a3b2ec7bb3fc11bf152404
Josh Brown <yes_you_do@hotmail.com>
2023-11-26 16:13:44 +0000

    Implement raw mode (with cooked fallback) for TTY initialisation

--- 2-Computer-Systems/problems/3-Beep-Beep-Boop/src/start.ts
+++ 2-Computer-Systems/problems/3-Beep-Beep-Boop/src/start.ts
@@ -1,4 +1,8 @@
-import { stdin, stdout } from 'node:process';
+import process, { stdin, stdout } from 'node:process';
+
+function isSigInt(buffer: Buffer) {
+  return buffer.length === 1 && buffer.readUInt8() === 3;
+}
 
 function isDigit(buffer: Buffer) {
   return buffer.length === 1 && 48 <= buffer.readUInt8() && 58 >= buffer.readUInt8();
@@ -18,13 +22,45 @@ function getASCIIBellBuffer(bellCount: number) {
   return Buffer.from(byteArray);
 }
 
-stdin.on('data', (buffer) => {
-  const firstKeyBuffer = buffer.subarray(0, 1);
+function initProgram(): void {
+  stdout.write('Ready to beep for your boop\n');
+
+  if (stdin.isTTY) {
+    stdin.setRawMode(true);
+  } else {
+    console.log(
+      '\nProcess not connected to a TTY',
+      "\nYour input is in 'cooked' mode and you'll have to press ENTER",
+      'to complete your input commands'
+    );
+  }
+}
+
+function handleDataInput(data: Buffer): void {
+  const firstKeyBuffer = stdin.isTTY ? data : data.subarray(0, 1);
+
   if (isDigit(firstKeyBuffer)) {
     const numKey = parseASCIIDigit(firstKeyBuffer);
     if (numKey) {
       stdout.write(getASCIIBellBuffer(numKey));
       console.log(new Array(numKey).fill('ding').join(' '));
     }
+    return;
   }
-});
+
+  if (isSigInt(firstKeyBuffer)) {
+    stdin.emit('SIGINT');
+    return;
+  }
+}
+
+function handleSIGINT(): void {
+  stdout.write('SIGINT Received! Press any key to exit.\n');
+  process.exitCode = 0;
+  stdin.removeAllListeners();
+}
+
+stdin.on('data', handleDataInput);
+stdin.on('SIGINT', handleSIGINT);
+
+initProgram();
