256d7fced79182e8f5e6ad5bc78c352a6ec3195a
Josh Brown <yes_you_do@hotmail.com>
2023-12-21 10:17:43 +0000

    Implement image metadata extraction

--- 2-Computer-Systems/problems/4-Image-Rotate/src/start.ts
+++ 2-Computer-Systems/problems/4-Image-Rotate/src/start.ts
@@ -1,4 +1,5 @@
 import { open } from 'node:fs/promises';
+import getBitmapMetadata from './fileHeader/getBitmapMetadata';
 
 function assertBitMapSignature(metadata: Buffer): void {
   const sig = metadata.subarray(0, 2);
@@ -12,6 +13,8 @@ const readTeapot = async () => {
   const metadataBuffer = Buffer.alloc(38);
   const { bytesRead } = await teapotFile.read({ buffer: metadataBuffer, length: 38 });
   assertBitMapSignature(metadataBuffer);
+  const { imageSize, imageXPixels, imageYPixels, bitsPerPixel, pixelArrayByteOffset } =
+    getBitmapMetadata(metadataBuffer);
 };
 
 readTeapot();
