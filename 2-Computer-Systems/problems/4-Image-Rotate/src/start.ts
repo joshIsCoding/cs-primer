09061278cc1a2d40a7929f844a08253d451311a0
Josh Brown <yes_you_do@hotmail.com>
2023-12-30 18:11:22 +0000

    Implement manual rotated teapot file generatin

--- 2-Computer-Systems/problems/4-Image-Rotate/src/start.ts
+++ 2-Computer-Systems/problems/4-Image-Rotate/src/start.ts
@@ -2,17 +2,40 @@ import { open } from 'node:fs/promises';
 import parseBitmapMetadata from './fileHeader/parseBitmapMetadata';
 import getPixelArray from './pixelArray/getPixelArrray';
 import getBitmapHeader from './fileHeader/getBitmapHeader';
+import getRotatedPixelArray from './pixelArray/getRotatedPixelArray';
+import getBitmapFooter from './fileFooter/getBitmapFooter';
 
 const readTeapot = async () => {
   const teapotFile = await open('helpfiles/teapot.bmp');
   const fileHeader = await getBitmapHeader(teapotFile);
-  const { imageSize, imageXPixels, imageYPixels, bitsPerPixel, pixelArrayOffset } =
+  const { fileSize, imageSize, imageWidth, imageHeight, bitsPerPixel, pixelArrayOffset } =
     parseBitmapMetadata(fileHeader);
   const pixelArray = await getPixelArray({
     bitmapFile: teapotFile,
     bytes: imageSize,
     pixelArrayOffset,
   });
+  const rotatedPixelArray = getRotatedPixelArray({
+    pixelArray,
+    imageWidth,
+    imageHeight,
+    bitsPerPixel,
+  });
+
+  const rotatedTeapotFile = await open('helpfiles/my_rotated_teapot.bmp', 'wx');
+  await rotatedTeapotFile.write(fileHeader);
+  await rotatedTeapotFile.write(rotatedPixelArray, null, null, pixelArrayOffset);
+  const rotatedTeapotFooter = await getBitmapFooter({
+    file: rotatedTeapotFile,
+    fileSize,
+    imageSize,
+    pixelArrayOffset,
+  });
+  if (rotatedTeapotFooter)
+    await rotatedTeapotFile.write(rotatedTeapotFooter, null, null, pixelArrayOffset + imageSize);
+
+  teapotFile.close();
+  rotatedTeapotFile.close();
 };
 
 readTeapot();
