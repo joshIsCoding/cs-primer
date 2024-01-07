0c81e9ac07431a191c8f81fb5e91e1c8010669ca
Josh Brown <yes_you_do@hotmail.com>
2024-01-07 20:39:41 +0000

    Modify endianness parsing to include sub-seconds unit extraction

--- 2-Computer-Systems/problems/5-Syn-Flood/src/start.ts
+++ 2-Computer-Systems/problems/5-Syn-Flood/src/start.ts
@@ -1,17 +1,24 @@
 import { open, readFile } from 'fs/promises';
 
-const LE_MAGIC_STRING_UINT = 2712847316;
-const BE_MAGIC_STRING_UINT = 3569595041;
+const LE_MICRO_MAGIC_STRING_UINT = 2712847316;
+const BE_MICRO_MAGIC_STRING_UINT = 3569595041;
+const LE_NANO_MAGIC_STRING_UINT = 2712812621;
+const BE_NANO_MAGIC_STRING_UINT = 1295823521;
 
 type Endianness = 'BE' | 'LE';
+type SubSecondsType = 'micro' | 'nano';
 
-const getPCapEndianess = (fileBuffer: Buffer): Endianness => {
+const getPCapMetadata = (fileBuffer: Buffer): [Endianness, SubSecondsType] => {
   const magicStringUInt = fileBuffer.readUInt32LE(0);
   switch (magicStringUInt) {
-    case LE_MAGIC_STRING_UINT:
-      return 'LE';
-    case BE_MAGIC_STRING_UINT:
-      return 'BE';
+    case LE_MICRO_MAGIC_STRING_UINT:
+      return ['LE', 'micro'];
+    case BE_MICRO_MAGIC_STRING_UINT:
+      return ['BE', 'micro'];
+    case LE_NANO_MAGIC_STRING_UINT:
+      return ['LE', 'nano'];
+    case BE_NANO_MAGIC_STRING_UINT:
+      return ['BE', 'nano'];
     default:
       throw new Error(`PCap file has unrecognised magic string ${magicStringUInt.toString(16)}`);
   }
@@ -31,7 +38,7 @@ async function start() {
   const pcapFile = await open('helpfiles/synflood.pcap');
   const packetStream = await readFile(pcapFile);
 
-  const fileEndianness = getPCapEndianess(packetStream);
+  const [fileEndianness, timestampSecondsFormat] = getPCapMetadata(packetStream);
   const linkHeaderType = getLinkHeaderType(packetStream, fileEndianness);
 
   if (linkHeaderType !== 0) throw new Error(`Unsupported Link Header format ${linkHeaderType}`);
