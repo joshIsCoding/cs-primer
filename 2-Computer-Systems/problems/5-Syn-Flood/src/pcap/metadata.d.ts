658a8b6200933f9bd50750f1bd5fc89663a9962c
Josh Brown <yes_you_do@hotmail.com>
2024-01-13 15:18:03 +0000

    Define PcapMetadata interface

--- 2-Computer-Systems/problems/5-Syn-Flood/src/pcap/metadata.d.ts
+++ 2-Computer-Systems/problems/5-Syn-Flood/src/pcap/metadata.d.ts
@@ -1,2 +1,8 @@
 export type Endianness = 'BE' | 'LE';
 export type SubSecondsType = 'micro' | 'nano';
+
+export interface PcapMetadata {
+  endianness: Endianness;
+  subSecondsType: SubSecondsType;
+  linkLayerProtocol: LinkLayerProtocol;
+}
