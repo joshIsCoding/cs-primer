98f5473d265d2f16311af4eda09eabaa9280eeb8
Josh Brown <yes_you_do@hotmail.com>
2024-01-13 15:16:36 +0000

    Implement base GetPacketRecordAtOffsetArgs and fix byte size in family key LE data read

--- 2-Computer-Systems/problems/5-Syn-Flood/src/packetRecord/linkLayer/getLinkLayerAddressFamily.ts
+++ 2-Computer-Systems/problems/5-Syn-Flood/src/packetRecord/linkLayer/getLinkLayerAddressFamily.ts
@@ -1,27 +1,18 @@
-import { Endianness } from '../../pcap/metadata';
-import { RECORD_HEADER_LENGTH } from '../recordHeader/getRecordHeaderAtOffset';
+import { GetPacketRecordAtOffsetArgs } from '../getPacketRecordAtOffset';
 import getLinkTypeNullAddressFamily from './nullLoopback/getLinkTypeNullAddressFamily';
 
-interface getLinkLayerAddressFamilyArgs {
-  pcapBuffer: Buffer;
-  recordOffset: number;
-  linkLayerProtocol: number;
-  endianness: Endianness;
-}
+type getLinkLayerAddressFamilyArgs = Omit<GetPacketRecordAtOffsetArgs, 'subSecondsType'>;
 
 const getLinkLayerAddressFamily = ({
   pcapBuffer,
   endianness,
-  recordOffset,
+  offset,
   linkLayerProtocol,
 }: getLinkLayerAddressFamilyArgs) => {
   switch (linkLayerProtocol) {
     case 0:
-      const linkLayerHeaderOffset = recordOffset + RECORD_HEADER_LENGTH;
       const addressFamilyKey =
-        endianness === 'BE'
-          ? pcapBuffer.readUInt32BE(linkLayerHeaderOffset)
-          : pcapBuffer.readUInt16LE(linkLayerHeaderOffset);
+        endianness === 'BE' ? pcapBuffer.readUInt32BE(offset) : pcapBuffer.readUInt32LE(offset);
       return getLinkTypeNullAddressFamily(addressFamilyKey);
     default:
       throw new Error(`Unsupported Link Header format ${linkLayerProtocol}`);
