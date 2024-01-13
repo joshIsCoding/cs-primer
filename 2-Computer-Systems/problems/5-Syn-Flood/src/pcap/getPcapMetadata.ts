f13c8cc6e354dd258c9112c5d5a83d38cbc2d324
Josh Brown <yes_you_do@hotmail.com>
2024-01-13 15:18:30 +0000

    Implement new Metadata interface and stricter link layer protocol derivation

--- 2-Computer-Systems/problems/5-Syn-Flood/src/pcap/getPcapMetadata.ts
+++ 2-Computer-Systems/problems/5-Syn-Flood/src/pcap/getPcapMetadata.ts
@@ -1,4 +1,8 @@
-import { Endianness, SubSecondsType } from './metadata';
+import {
+  LinkLayerProtocol,
+  isLinkLayerProtocol,
+} from '../packetRecord/linkLayer/linkLayerProtocols';
+import { Endianness, PcapMetadata, SubSecondsType } from './metadata';
 
 const LE_MICRO_MAGIC_STRING_UINT = 2712847316;
 const BE_MICRO_MAGIC_STRING_UINT = 3569595041;
@@ -7,18 +11,15 @@ const BE_NANO_MAGIC_STRING_UINT = 1295823521;
 
 const LINK_HEADER_OFFSET = 22;
 
-interface PcapMetadata {
-  endianness: Endianness;
-  subSecondsType: SubSecondsType;
-  linkLayerProtocol: number;
-}
+const getlinkLayerProtocoll = (fileBuffer: Buffer, endianness: Endianness): LinkLayerProtocol => {
+  const protocol =
+    endianness === 'BE'
+      ? fileBuffer.readUInt16BE(LINK_HEADER_OFFSET)
+      : fileBuffer.readUInt16LE(LINK_HEADER_OFFSET);
 
-const getlinkLayerProtocoll = (fileBuffer: Buffer, endianness: Endianness): number => {
-  if (endianness === 'BE') {
-    return fileBuffer.readUInt16BE(LINK_HEADER_OFFSET);
-  } else {
-    return fileBuffer.readUInt16LE(LINK_HEADER_OFFSET);
-  }
+  if (isLinkLayerProtocol(protocol)) return protocol;
+
+  throw new Error(`Unsupported Link Layer protocol ${protocol}`);
 };
 
 const parseMagicString = (fileBuffer: Buffer): [Endianness, SubSecondsType] => {
