b395277bb4b9552971a1cfe5c350b33543c3092c
Josh Brown <yes_you_do@hotmail.com>
2024-01-14 18:50:18 +0000

    Implement initial grouping of TCP packets by source/destination address pairs

---
+++ 2-Computer-Systems/problems/5-Syn-Flood/src/analysis/tcp/buildTCPConnectionChain.ts
@@ -0,0 +1,37 @@
+import { PacketRecord } from '../../packetRecord/packetRecord';
+import { TCPSegmentHeader } from '../../packetRecord/payload/packetBody/tcp/tcpSegmentHeader';
+import { Address, AddressPairKey } from './tcpConnectionChain';
+
+const buildAddress = (ip: number, port: number): Address => `${ip}:${port}`;
+
+type TCPAddressGetter = (packet: PacketRecord<TCPSegmentHeader>) => Address;
+
+const getSourceAddress: TCPAddressGetter = ({ packetBody, packetHeader }) =>
+  buildAddress(packetHeader.rawSource, packetBody.sourcePort);
+
+const getDestinationAddress: TCPAddressGetter = ({ packetBody, packetHeader }) =>
+  buildAddress(packetHeader.rawDestination, packetBody.destinationPort);
+
+const getAddressPairKey = (packet: PacketRecord<TCPSegmentHeader>): AddressPairKey => {
+  const sourceAddress = getSourceAddress(packet);
+  const destinationAddress = getDestinationAddress(packet);
+
+  if (sourceAddress < destinationAddress) return `${sourceAddress}-${destinationAddress}`;
+
+  return `${destinationAddress}-${sourceAddress}`;
+};
+
+const buildTCPConnectionChain = (
+  packets: PacketRecord<TCPSegmentHeader>[]
+): Record<AddressPairKey, PacketRecord[]> => {
+  const connectionChain: Record<AddressPairKey, PacketRecord[]> = {};
+  packets.forEach((packet) => {
+    const addressPairKey = getAddressPairKey(packet);
+    const priorPackets = connectionChain[addressPairKey] ?? [];
+    connectionChain[addressPairKey] = [...priorPackets, packet];
+  });
+
+  return connectionChain;
+};
+
+export default buildTCPConnectionChain;
