d939bc9e8406f7b4db1d60d6f16bf45cb48289ec
Josh Brown <yes_you_do@hotmail.com>
2024-01-21 13:35:42 +0000

    Create explicit SYN/ACK TCP segment subtypes and redefine general TCPSegmentHeader as union thereof

--- 2-Computer-Systems/problems/5-Syn-Flood/src/packetRecord/payload/packetBody/tcp/tcpSegmentHeader.d.ts
+++ 2-Computer-Systems/problems/5-Syn-Flood/src/packetRecord/payload/packetBody/tcp/tcpSegmentHeader.d.ts
@@ -1,11 +1,35 @@
 import { PacketProtocol } from '../packetProtocols';
 
-export interface TCPSegmentHeader extends PacketProtocol {
+interface BaseTCPSegmentHeader extends PacketProtocol {
   protocol: 'TCP';
   sourcePort: number;
   destinationPort: number;
   sequenceNumber: number;
   ackNumber: number;
-  synState: boolean;
-  ackState: boolean;
 }
+
+export interface SynTCPSegmentHeader extends BaseTCPSegmentHeader {
+  synState: true;
+  ackState: false;
+}
+
+export interface SynAckTCPSegmentHeader extends BaseTCPSegmentHeader {
+  synState: true;
+  ackState: true;
+}
+
+export interface AckTCPSegmentHeader extends BaseTCPSegmentHeader {
+  synState: false;
+  ackState: true;
+}
+
+interface NonSynAckSegmentHeader extends BaseTCPSegmentHeader {
+  synState: false;
+  ackState: false;
+}
+
+export type TCPSegmentHeader =
+  | SynTCPSegmentHeader
+  | SynAckTCPSegmentHeader
+  | AckTCPSegmentHeader
+  | NonSynAckSegmentHeader;
