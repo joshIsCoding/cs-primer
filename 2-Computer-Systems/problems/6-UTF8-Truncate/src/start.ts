cd8c325466647bd042588a0a59c86ee41348311d
Josh Brown <yes_you_do@hotmail.com>
2024-03-04 11:48:34 +0000

    Implement zero-width joiner grapheme cluster rule, write to STDOUT instead of file, and refactor

--- 2-Computer-Systems/problems/6-UTF8-Truncate/src/start.ts
+++ 2-Computer-Systems/problems/6-UTF8-Truncate/src/start.ts
@@ -1,7 +1,8 @@
 import { open } from 'fs/promises';
 
-const LINE_FEED_CHAR = 10;
-const OUTPUT_FILENAME = 'truncated_lines.txt';
+const LINE_FEED_CHAR = 0x0a;
+const CARRIAGE_RETURN_CHAR = 0x0d;
+const ZERO_WIDTH_JOINER_BUFFER = Buffer.from([0xe2, 0x80, 0x8d]);
 
 const assertSingleByte = (byte: number): void => {
   if (byte > 255 || byte < 0) {
@@ -11,46 +12,59 @@ const assertSingleByte = (byte: number): void => {
 
 type ByteCharPredicate = (byte: number) => boolean;
 
-const isContinuationByte: ByteCharPredicate = (byte) => (byte & 0xc0) === 0x80;
-
-const isSingleByteChar: ByteCharPredicate = (byte) => (byte & 0x80) === 0;
-
-const isFirstOf2ByteChar: ByteCharPredicate = (byte) => (byte & 0xe0) === 0xc0;
-
-const isFirstOf3ByteChar: ByteCharPredicate = (byte) => (byte & 0xf0) === 0xe0;
-
-const isFirstOf4ByteChar: ByteCharPredicate = (byte) => (byte & 0xf8) === 0xf0;
-
-const isFirstByteOfChar: ByteCharPredicate = (byte) => {
+const isContinuationByte: ByteCharPredicate = (byte) => {
   assertSingleByte(byte);
 
-  return [isSingleByteChar, isFirstOf2ByteChar, isFirstOf3ByteChar, isFirstOf4ByteChar].some(
-    (predicate) => predicate(byte)
-  );
+  return (byte & 0xc0) === 0x80;
 };
 
-const getIndexOfFirstByteOfLastChar = (bytes: Buffer): number => {
-  for (let i = bytes.byteLength - 1; i >= bytes.byteLength - 5; i--) {
+const isZeroWidthJoiner = (threeByteBuffer: Buffer): boolean => {
+  if (threeByteBuffer.byteLength !== 3)
+    throw new Error(`Buffer ${threeByteBuffer.toString('hex')} is not three bytes long`);
+
+  return threeByteBuffer.equals(ZERO_WIDTH_JOINER_BUFFER);
+};
+
+const getSafeTruncationByteLength = (bytes: Buffer, targetByteLength: number): number => {
+  if (bytes.byteLength < targetByteLength) return bytes.byteLength;
+
+  let firstOmittedCharOffset: number | undefined;
+  // start reading at 1 byte over target length
+  for (let i = targetByteLength; i >= targetByteLength - 4; i--) {
     const currentByte = bytes[i];
 
-    if (isFirstByteOfChar(currentByte)) return i;
+    if (!isContinuationByte(currentByte)) {
+      firstOmittedCharOffset = i;
+      break;
+    }
   }
-  throw new Error(`No UTF-8 first bytes found in ${bytes}`);
+  if (firstOmittedCharOffset === undefined) {
+    throw new Error(`Unexpected UTF-8 byte pattern found in ${bytes.toString('utf-8')}`);
+  }
+
+  const last3Bytes = bytes.subarray(firstOmittedCharOffset - 3, firstOmittedCharOffset);
+  const omitted3Bytes = bytes.subarray(firstOmittedCharOffset, firstOmittedCharOffset + 3);
+
+  if (last3Bytes.byteLength === 3 && isZeroWidthJoiner(last3Bytes)) {
+    return getSafeTruncationByteLength(bytes, Math.max(firstOmittedCharOffset - 3, 0));
+  }
+  if (omitted3Bytes.byteLength === 3 && isZeroWidthJoiner(omitted3Bytes)) {
+    return getSafeTruncationByteLength(bytes, firstOmittedCharOffset - 1);
+  }
+
+  return firstOmittedCharOffset;
 };
 
 const truncateUTF8Buffer = (untruncatedBuffer: Buffer, targetByteLength: number): Buffer => {
-  const maxBufferLength = Math.min(untruncatedBuffer.byteLength, targetByteLength);
-  const truncatedBuffer = Buffer.alloc(maxBufferLength + 1);
+  const safeBufferLength = getSafeTruncationByteLength(untruncatedBuffer, targetByteLength);
+  const truncatedBuffer = Buffer.alloc(safeBufferLength);
   untruncatedBuffer.copy(truncatedBuffer);
-  // this length will always be <= targetByteLength
-  const safeBufferLength = getIndexOfFirstByteOfLastChar(truncatedBuffer);
 
-  return truncatedBuffer.subarray(0, safeBufferLength);
+  return truncatedBuffer;
 };
 
 const runCaseFileTruncation = async () => {
   const caseFile = await open('helpfiles/cases', 'r');
-  const outputFile = await open(OUTPUT_FILENAME, 'w');
   const truncatedLines: Buffer[] = [];
   const fileBuffer = await caseFile.readFile();
 
@@ -69,17 +83,18 @@ const runCaseFileTruncation = async () => {
       // skip up to 2 bytes - one for any previous EOL and one for the truncation integer
       const startSlice = lastEOLOffset === undefined ? 1 : lastEOLOffset + 2;
       const lineToTruncate = fileBuffer.subarray(startSlice, currentByteOffset);
-      truncatedLines.push(truncateUTF8Buffer(lineToTruncate, bytesToTruncate));
+
+      const truncatedLine = truncateUTF8Buffer(lineToTruncate, bytesToTruncate);
+      truncatedLines.push(truncatedLine);
       truncatedLines.push(Buffer.from([LINE_FEED_CHAR]));
+
       lastEOLOffset = currentByteOffset;
     }
     currentByteOffset++;
   }
 
-  await outputFile.writeFile(Buffer.concat(truncatedLines));
-  console.log(`-> Truncated lines saved to ${OUTPUT_FILENAME}`);
+  process.stdout.write(Buffer.concat(truncatedLines));
 
-  await outputFile.close();
   await caseFile.close();
 };
 
