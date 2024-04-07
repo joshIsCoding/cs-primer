7c0830d9bc0be0c2819c3950f2523555334f208c
Josh Brown <yes_you_do@hotmail.com>
2024-04-07 21:04:34 +0100

    Implement bitVector conversions

---
+++ 2-Computer-Systems/problems/7-Sneaky-NaN/src/start.ts
@@ -0,0 +1,33 @@
+import getASCIICodes from './ascii/getASCIICodes';
+import getStringFromASCIICodes from './ascii/getStringFromASCIICodes';
+import getASCIIFromBitVectorInt from './ascii/bitVector/getASCIIFromBitVectorInt';
+import getBitVectorIntFromASCII from './ascii/bitVector/getBitVectorIntFromASCII';
+
+function encode(message: string): bigint {
+  return getBitVectorIntFromASCII(getASCIICodes(message));
+}
+
+function decode(bitVectorInt: bigint): string {
+  return getStringFromASCIICodes(getASCIIFromBitVectorInt(bitVectorInt));
+}
+
+const hey = 'HEYA!';
+
+console.log('Input:', hey);
+console.log('Encoded:', encode(hey));
+console.log('Decoded:', decode(encode(hey)));
+
+// const nanArray = new Int8Array(8);
+// nanArray[0] = -1;
+// nanArray[1] = -16;
+
+// nanArray.buffer;
+
+// const buffer = new ArrayBuffer(8);
+// const viewBE = new DataView(buffer);
+// viewBE.setFloat64(0, Number.NaN, false);
+
+// // []
+// // 11110000
+// // -(2^7) + 2^6 + 2^5 + 2^4
+// console.log(buffer);
