76fd801e326e9c1b354812d23fc1f8c35433e804
Josh Brown <yes_you_do@hotmail.com>
2024-04-07 22:02:35 +0100

    Rename helpers to match challenge and implement verbose demo of problem example

--- 2-Computer-Systems/problems/7-Sneaky-NaN/src/start.ts
+++ 2-Computer-Systems/problems/7-Sneaky-NaN/src/start.ts
@@ -5,14 +5,14 @@ import getBitVectorIntFromASCII from './ascii/bitVector/getBitVectorIntFromASCII
 import buildNaN from './nan/buildNaN';
 import parseNaN from './nan/parseNaN';
 
-function encode(message: string): number {
+function conceal(message: string): number {
   const asciiSeq = getASCIICodes(message);
   const bitVector = getBitVectorIntFromASCII(asciiSeq);
 
   return buildNaN(bitVector);
 }
 
-function decode(nan: number): string {
+function extract(nan: number): string {
   const bitVector = parseNaN(nan);
   const asciiSeq = getASCIIFromBitVectorInt(bitVector);
 
@@ -22,5 +22,17 @@ function decode(nan: number): string {
 const testMsg = 'ïœßïœßïœß';
 
 console.log('Input:  ', `\`${testMsg}\``);
-console.log('Encoded:', encode(testMsg));
-console.log('Decoded:', `\`${decode(encode(testMsg))}\``);
+console.log('Encoded:', conceal(testMsg));
+console.log('Decoded:', `\`${extract(conceal(testMsg))}\``);
+
+let x = conceal('hello!');
+console.log("\nlet x = conceal('hello!')");
+console.log('Encoded x:', x);
+console.log('typeof x: ', typeof x);
+
+console.log('x + 5:    ', x + 5);
+console.log('x / 2:    ', x / 2);
+console.log('x += 3;');
+x += 3;
+
+console.log('Decoded x:', extract(x));
