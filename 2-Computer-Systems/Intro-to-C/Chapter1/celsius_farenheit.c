b446a2c7aace02a2535356bc5014da581c69466a
Josh Brown <yes_you_do@hotmail.com>
2024-05-04 15:56:00 +0100

    Write simple celsius-fahrenheit table printer

---
+++ 2-Computer-Systems/Intro-to-C/Chapter1/celsius_farenheit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* Prints a table of values reflecting the corresponding temperatures in degrees
+   Fahrenheit and Celsius */
+int main() {
+  int fahren, celsius; // temperature values in fahrenheit and celsius
+  int lower, upper, step;
+
+  lower = 0; // lower bound for fahrenheit
+  upper = 300; // upper bound for fahrenheit
+  step = 20; // increment for each row of table
+
+  fahren = lower;
+
+  printf("Fahrenheit - Celsius (%dF to %dF)\n", lower, upper);
+  printf("---------------------------------\n");
+
+  while(fahren <= upper) {
+    celsius = 5 * (fahren - 32) / 9;
+
+    printf("%d\t\t%d\n", fahren, celsius);
+    fahren += step;
+  }
+}
