3430fa3835c765b07c79c7bb4428d272f0e75358
Josh Brown <yes_you_do@hotmail.com>
2024-05-04 18:30:17 +0100

    Add alternate for-loop construction in a doc-comment

--- 2-Computer-Systems/Intro-to-C/Chapter1/count_chars.c
+++ 2-Computer-Systems/Intro-to-C/Chapter1/count_chars.c
@@ -8,5 +8,13 @@ int main()
   while (getchar() != EOF)
     ++nc;
 
+  /* could be written as the following:
+
+    for (nc = 0; getchar() != EOF; ++nc)
+      ;  // this is a null-statement
+
+    It's required as for-loops require at least one consecutive statement
+    */
+
   printf("Number of chars: %ld\n", nc);
 }
