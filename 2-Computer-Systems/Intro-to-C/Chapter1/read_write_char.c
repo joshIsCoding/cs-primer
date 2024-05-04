e5c89f34e903b209718a6dff9b5d71a227ee9d7d
Josh Brown <yes_you_do@hotmail.com>
2024-05-04 16:59:34 +0100

    Implement first version of single-char reading and writing

---
+++ 2-Computer-Systems/Intro-to-C/Chapter1/read_write_char.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Read single-character input then immediately output that character */
+int main() {
+  int c; /* we use int because EOF (defined in stdio.h) is a special, hardware dependent
+            integer that will generally exceed the capacity of the char type. Of course, any 
+            character is just a bit pattern, so int suffices */
+
+  c = getchar();
+  while(c != EOF) {
+    putchar(c);
+    c = getchar();
+  }
+
+}
