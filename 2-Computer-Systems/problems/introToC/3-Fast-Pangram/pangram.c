6d02321adb9b93f321b0f90c62739a393cfafe39
Josh Brown <yes_you_do@hotmail.com>
2025-11-23 14:48:25 +0000

    Implement initial pangram.c

--- 2-Computer-Systems/problems/introToC/3-Fast-Pangram/pangram.c
+++ 2-Computer-Systems/problems/introToC/3-Fast-Pangram/pangram.c
@@ -2,8 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define COMPLETE_ALPHA_SET 67108863 /* 2^26 */
 
 bool ispangram(char *s) {
+  // A bit vector with 26 significant bits, where each represents inclusion in
+  // the english alphabet
+  unsigned long alpha_set = 0;
+  // alpha_index is a value where 1 <= alpha_index <= 26 representing the
+  // corresponding case-normalsed letter of the english alphabet.
+  unsigned int i = 0, c = 0, alpha_index = 0;
+
+  while ((c = s[i]) != '\n' && c != '\0') {
+    i++;
+
+    if (c >= 'a' && c <= 'z') {
+      alpha_index = (c - 'a');
+    } else if (c >= 'A' && c <= 'Z') {
+      alpha_index = (c - 'A');
+    } else {
+      continue;
+    }
+    alpha_set |= (1 << alpha_index);
+  }
+
+  if (alpha_set == COMPLETE_ALPHA_SET) {
+    return true;
+  }
+
   // TODO implement this!
   return false;
 }
